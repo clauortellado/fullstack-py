056a18d0987b28ed43e2fb918fe822fb27b9749b
KlauZen <45660061+clauortellado@users.noreply.github.com>
2020-11-10 19:18:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log(parseInt("F",16));
-console.log(parseInt("15"));
-console.log("texto ");
-
-document.write("<h1>Curso de JavaScript 1</h1>");
-document.write("<p>Python</p>")
-
-alert("Practice Practice Practice!")
